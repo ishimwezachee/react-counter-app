2c6ea19468e7d13afd092591ae4849a3e3facfc1
zachee <zacheeishimwe@gmail.com>
2020-03-14 11:29:41 +0200

    number of counters on the navbar

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ class App extends Component {
   render() { 
     return ( 
       <React.Fragment>
-      <NavBar />
+      <NavBar 
+      totalCounters={this.state.counters.filter(c=>c.value>0).length} 
+      />
     <main className="container">
       <Counters
       counters= {this.state.counters} 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ class NavBar extends Component {
         return (
             <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
-            Navbar
+            Navbar  
+            <span className = "badge badge-pill badge-secondary" >
+                {this.props.totalCounters}
+            </span>
            </a>
 </nav>
           );
