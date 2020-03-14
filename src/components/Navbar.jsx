9b4bcb2c60ec844d3ae67f4a1163169de3b6e16a
zachee <zacheeishimwe@gmail.com>
2020-03-14 12:00:38 +0200

    destructuring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 // stateless Functional Component
 // u can use function when you are dealing with simple stateless component
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
     return (
             <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
             Navbar  
             <span className = "badge badge-pill badge-secondary" >
-                {props.totalCounters}
+                {totalCounters}
             </span>
            </a>
 </nav>
